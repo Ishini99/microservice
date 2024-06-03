ce42628b54e8cb5b9891024ff2d005578497f3cb
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-06-03 22:58:00 +0530

    updated order service

---
+++ order-service/src/test/java/com/shoppingApplication/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingApplication.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
