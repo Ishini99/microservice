91d008c3873be3eafa8379705182397a6d45fbc0
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-07-02 12:28:33 +0530

    setup inventry-service

---
+++ inverntary-service/src/main/java/com/shoppingApplication/inverntary_service/InverntaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingApplication.inverntary_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InverntaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InverntaryServiceApplication.class, args);
+	}
+
+}
---
+++ inverntary-service/src/test/java/com/shoppingApplication/inverntary_service/InverntaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingApplication.inverntary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InverntaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
