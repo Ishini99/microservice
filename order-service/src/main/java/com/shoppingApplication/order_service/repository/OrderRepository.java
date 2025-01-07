79f2f11ac9cc02e81ce7ba78c6d6cedb3ff66c31
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-01-07 13:36:39 +0530

    updated orderRepo

--- order-service/src/main/java/com/shoppingApplication/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/shoppingApplication/order_service/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.shoppingApplication.order_service.repository;
 import com.shoppingApplication.order_service.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+@Repository
+public interface OrderRepo extends JpaRepository<Orders, Integer> {
+    @Query(value = "SELECT * FROM order WHERE id = ?1", nativeQuery = true)
+    Orders getOrderById(Integer orderId);
 }
+
