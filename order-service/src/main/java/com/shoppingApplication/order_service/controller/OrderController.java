0cd070f173eb56e1467961cb3d2718cb6ff195b8
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-01-07 13:25:27 +0530

    updated order controller

--- order-service/src/main/java/com/shoppingApplication/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/shoppingApplication/order_service/controller/OrderController.java
@@ -1,4 +1,4 @@
-package com.shoppingApplication.order_service.controller;
+gpackage com.shoppingApplication.order_service.controller;
 
 import com.shoppingApplication.order_service.dto.OrderRequest;
 import com.shoppingApplication.order_service.repository.OrderRepository;
@@ -8,15 +8,39 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/order")
-@RequiredArgsConstructor
+@RequestMapping("api/v1")
+@CrossOrigin
 public class OrderController {
-    private final OrderService orderService;
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order placed Successfully";
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderProducer orderProducer;    @PostMapping("/addorder")
+    public OrderResponse saveOrder(@RequestBody OrderDTO orderDTO) {
+        OrderEventDTO orderEventDTO = new OrderEventDTO();
+        orderEventDTO.setMessage("Order is committed");
+        orderEventDTO.setStatus("pending");
+        orderProducer.sendMessage(orderEventDTO);
+
+        return orderService.saveOrder(orderDTO);
+    }
+    @GetMapping("/getorders")
+    public List<OrderDTO> getOrders() {
+        return orderService.getAllOrders();
+    } 
+    
+    @GetMapping("/order/{orderId}")
+    public OrderDTO getOrderById(@PathVariable Integer orderId) {
+        return orderService.getOrderById(orderId);
     }
 
+    @PutMapping("/updateorder")
+    public OrderDTO updateOrder(@RequestBody OrderDTO orderDTO) {
+        return orderService.updateOrder(orderDTO);
+    }
+
+    @DeleteMapping("/deleteorder/{orderId}")
+    public String deleteOrder(@PathVariable Integer orderId) {
+        return orderService.deleteOrder(orderId);
+    }
 }
